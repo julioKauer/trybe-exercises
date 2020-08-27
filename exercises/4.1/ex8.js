23e280b759c022d50603bd50f42be6f851f34c00
Julio Kauer <juliokauer@gmail.com>
2020-08-27 18:51:13 -0300

    ex7 ex8 ex9

---
+++ exercises/4.1/ex8.js
@@ -0,0 +1,9 @@
+var a = 1;
+var b = 3;
+var c = 5;
+
+thereIsAEven = a % 2 == 0 || b % 2 == 0 || c % 2 == 0;
+
+console.log( thereIsAEven );
+
+// Bônus extra : nenhum if
---
+++ exercises/4.1/ex9.js
@@ -0,0 +1,9 @@
+var a = 2;
+var b = 4;
+var c = 6;
+
+thereIsAOdd = a % 2 == 1 || b % 2 == 1 || c % 2 == 1;
+
+console.log( thereIsAOdd );
+
+// Bônus extra : nenhum if
