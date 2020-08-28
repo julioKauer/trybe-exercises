dd8ff80b68d05fc7db6b680d4662ab1c2a8d1ca2
Julio Kauer <juliokauer@gmail.com>
2020-08-28 15:50:21 -0300

    Ex5 e ex6

--- exercises/4_2/ex6.js
+++ exercises/4_2/ex6.js
@@ -1,1 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let odds = 0;
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 == 1) {
+    odds++;
+  }
+}
+if (odds == 0) {
+  console.log("nenhum valor ímpar encontrado");
+} else {
+  console.log("Foram encontrados " + odds + " números ímpares.");
+}
