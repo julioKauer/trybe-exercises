b192eb0fd8bbcd5345155bc095a5fe1867f9d1f9
Julio Kauer <juliokauer@gmail.com>
2020-08-28 18:49:08 -0300

    Final do dia

--- exercises/4_2/ex9.js
+++ exercises/4_2/ex9.js
@@ -1,1 +1,7 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let array = [];
+for (let index = 0; index < 25; index++) {
+    array[index] = index + 1;
+}
+for (let index = 0; index < array.length; index++) {
+    console.log( array[index] / 2 );
+}
