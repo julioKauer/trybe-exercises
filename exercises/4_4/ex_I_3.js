e79298ab230794af4d2b169e5fa6216c0aac2071
Julio Kauer <juliokauer@gmail.com>
2020-09-01 16:02:21 -0300

    Part I

--- exercises/4_4/ex_I_3.js
+++ exercises/4_4/ex_I_3.js
@@ -3,3 +3,8 @@ let info = {
   origem: "Pato Donald",
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
 };
+
+info["recorrente"] = "Sim";
+for (let key in info) {
+  console.log(key);
+}
--- exercises/4_4/ex_I_5.js
+++ exercises/4_4/ex_I_5.js
@@ -1,5 +1,24 @@
-let info = {
-  personagem: "Margarida",
-  origem: "Pato Donald",
-  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
-};
+let info = [
+  {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim"
+  },
+  {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "Sim"
+  }
+];
+
+for (key in info[0]){
+  if (key == "recorrente" && info[0][key] == "Sim" && info[1][key] == "Sim") {
+    console.log("Ambos recorrentes");
+  } else if (key == "recorrente" && info[0][key] == "Não" && info[1][key] == "Não") {
+    console.log("Nenhum recorrente");
+  } else {
+    console.log(`${info[0][key]} e ${info[1][key]}`)
+  }
+}
