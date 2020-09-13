d74610699a17cae47f87db2485979ace46dc36d2
Julio Kauer <juliokauer@gmail.com>
2020-09-13 18:25:30 -0300

    Nem sei

--- exercises/5_4/script.js
+++ exercises/5_4/script.js
@@ -1,11 +1,11 @@
 window.onload = function () {
-  let backgroundColorSelector = document.querySelector("#background-color");
-  let fontColorSelector = document.querySelector("#color");
-  let fontSizeSelector = document.querySelector("#font-size");
-  let lineHeightSelector = document.querySelector("#font-spacing");
-  let fontFamilySelector = document.querySelector("#font-family");
-  let body = document.querySelector("body");
-  let article = document.querySelector("article");
+  let backgroundColorSelector = document.querySelector('#background-color');
+  let fontColorSelector = document.querySelector('#color');
+  let fontSizeSelector = document.querySelector('#font-size');
+  let lineHeightSelector = document.querySelector('#font-spacing');
+  let fontFamilySelector = document.querySelector('#font-family');
+  let body = document.querySelector('body');
+  let article = document.querySelector('article');
 
   if (localStorage.backgroundColor !== undefined) {
     body.style.backgroundColor = `${localStorage.backgroundColor}`;
@@ -20,11 +20,11 @@ window.onload = function () {
     lineHeightSelector.value = parseFloat(localStorage.lineHeight);
     fontFamilySelector.value = `${localStorage.fontFamily}`;
   } else {
-    localStorage.setItem("backgroundColor", "#ffffff");
-    localStorage.setItem("color", "#000000");
-    localStorage.setItem("fontSize", "16px");
-    localStorage.setItem("lineHeight", "1.5em");
-    localStorage.setItem("fontFamily", "serif");
+    localStorage.setItem('backgroundColor', '#ffffff');
+    localStorage.setItem('color', '#000000');
+    localStorage.setItem('fontSize', '16px');
+    localStorage.setItem('lineHeight', '1.5em');
+    localStorage.setItem('fontFamily', 'serif');
 
     backgroundColorSelector.value = `${localStorage.backgroundColor}`;
     fontColorSelector.value = `${localStorage.color}`;
@@ -33,23 +33,23 @@ window.onload = function () {
     fontFamilySelector.value = `${localStorage.fontFamily}`;
   }
 
-  backgroundColorSelector.addEventListener("change", function () {
+  backgroundColorSelector.addEventListener('change', function () {
     body.style.backgroundColor = `${backgroundColorSelector.value}`;
     localStorage.backgroundColor = `${backgroundColorSelector.value}`;
   });
-  fontColorSelector.addEventListener("change", function () {
+  fontColorSelector.addEventListener('change', function () {
     body.style.color = `${fontColorSelector.value}`;
     localStorage.color = `${fontColorSelector.value}`;
   });
-  fontSizeSelector.addEventListener("change", function () {
+  fontSizeSelector.addEventListener('change', function () {
     article.style.fontSize = `${fontSizeSelector.value}px`;
     localStorage.fontSize = `${fontSizeSelector.value}px`;
   });
-  lineHeightSelector.addEventListener("change", function () {
+  lineHeightSelector.addEventListener('change', function () {
     article.style.lineHeight = `${lineHeightSelector.value}em`;
     localStorage.lineHeight = `${lineHeightSelector.value}em`;
   });
-  fontFamilySelector.addEventListener("change", function () {
+  fontFamilySelector.addEventListener('change', function () {
     article.style.fontFamily = `${fontFamilySelector.value}`;
     localStorage.fontFamily = `${fontFamilySelector.value}`;
   });
