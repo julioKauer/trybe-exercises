3ca9973d921d0e2d4a6bb80cd9127985bbca4947
Julio Kauer <juliokauer@gmail.com>
2020-09-18 18:32:44 -0300

    Form

---
+++ exercises/6_1/script.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+  const states = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+  const selectState = document.querySelector('#state');
+
+  states.forEach((state) => {
+    const option = document.createElement('option');
+    option.innerHTML = state;
+    option.value = state;
+    selectState.appendChild(option);
+  });
+}
